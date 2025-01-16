104aadfa16813caf0e9cfec45c1d3f68555e92db
Sergei Kraev <serega.kraev1@yandex.ru>
2025-01-16 20:08:38 +0300

    add static folder and configure it

--- src/main/java/ru/yandex/practicum/WebConfiguration.java
+++ src/main/java/ru/yandex/practicum/WebConfiguration.java
@@ -3,9 +3,19 @@ package ru.yandex.practicum;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"ru.yandex.practicum"})
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/style/**")
+                .addResourceLocations("classpath:/static/style/");
+        registry.addResourceHandler("/static/script/**")
+                .addResourceLocations("classpath:/static/script/");
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/model/Post.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.model;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+    @NonNull
+    private Integer id;
+    @NonNull
+    private String name;
+    @NonNull
+    private String imageUrl;
+    @NonNull
+    private List<String> description;
+    @NonNull
+    private List<String> tags;
+    @NonNull
+    private List<Comment> comments;
+    @NonNull
+    private int likes;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) &&
+                Objects.equals(name, post.name) &&
+                Objects.equals(imageUrl, post.imageUrl) &&
+                Objects.equals(description, post.description) &&
+                Objects.equals(tags, post.tags);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, imageUrl, description, tags);
+    }
+}
