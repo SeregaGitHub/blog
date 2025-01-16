f9de0b0de1cc470d80b1678ee7a09ee5813f459e
Sergei Kraev <serega.kraev1@yandex.ru>
2025-01-16 18:14:14 +0300

    add web and data configuration

---
+++ src/main/java/ru/yandex/practicum/configuration/DataSourceConfiguration.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.configuration;
+
+import org.h2.Driver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    public DataSource dataSource(
+            @Value("jdbc:h2:mem:usersdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE") String url,
+            @Value("sa") String username,
+            @Value("password") String password
+    ) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Driver.class.getName());
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @EventListener
+    public void populate(ContextRefreshedEvent event) {
+        DataSource dataSource = event.getApplicationContext().getBean(DataSource.class);
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("schema.sql"));
+        populator.execute(dataSource);
+    }
+}
